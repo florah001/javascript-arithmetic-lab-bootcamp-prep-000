e1a34d9c7208cac6f66b3c54bbf7d84ffb337995
florah001 <florah001@yahoo.co.uk>
2018-10-07 20:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,23 +20,3 @@ function dec(a) {
 function makeInt(a) {
   return (a.toString());
 }
-/*
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal(.toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-*/
